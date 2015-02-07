e4d8cb8a0ed3c6dfd35cc1ce4b64c90b903247a1
Christopher Foo <chris.foo@gmail.com>
2015-02-07 14:55:23 -0500

    Fix ignoring related applications page
    Fixes repeatedly fetching 404 urls

--- ovi-store.lua
+++ ovi-store.lua
@@ -31,7 +31,10 @@ wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_pars
   end
   
   if item_type == "app" and (downloaded[url] ~= true and addedtolist[url] ~= true) then
-    if string.match(url, "[^0-9]"..item_value.."[0-9][^0-9]") and not string.match(url, "https?://store%.ovi%.com/content/"..item_value.."[0-9]/applications%?categoryId=[0-9]+") then
+    if string.match(url, "[^0-9]"..item_value.."[0-9][^0-9]") 
+    and not string.match(url, "https?://store%.ovi%.com/content/"..item_value.."[0-9]/.*applications%?categoryId=[0-9]+") 
+    and not string.match(url, "https?://store%.ovi%.com/content/"..item_value.."[0-9]/channel/channel/")
+    then
       return verdict
     elseif html == 0 then
       return verdict
@@ -48,7 +51,9 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
   local html = nil
   
   local function check(url)
-    if (downloaded[url] ~= true and addedtolist[url] ~= true) and not string.match(url, "https?://store%.ovi%.com/content/"..item_value.."[0-9]/applications%?categoryId=[0-9]+") then
+    if (downloaded[url] ~= true and addedtolist[url] ~= true) 
+    and not string.match(url, "https?://store%.ovi%.com/content/"..item_value.."[0-9]/.*applications%?categoryId=[0-9]+") 
+    and not string.match(url, "https?://store%.ovi%.com/content/"..item_value.."[0-9]/channel/channel/") then
       table.insert(urls, { url=url })
       addedtolist[url] = true
     end
